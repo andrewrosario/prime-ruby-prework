d826bf09602bb32f212232dbc21d4eefbe08171c
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-23 22:21:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,14 @@
 # Add  code here!
  
-prime_numbers = []
+ def prime?(n)
+   is_prime = true
+   range = 2..(Math.sqrt(n))
+   for number in range do
+     if n%number == 0 
+       is_prime = false
+     end
+   end
+    return is_prime
+  end
        
+   
