fbeac31c24dbf1f535b9d385e82760a09cc57588
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-23 22:22:34 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
 # Add  code here!
  
  def prime?(n)
+   num = n.abs
    is_prime = true
-   range = 2..(Math.sqrt(n))
+   range = 2..(Math.sqrt(num))
    for number in range do
-     if n%number == 0 
+     if num%number == 0 
        is_prime = false
      end
    end
